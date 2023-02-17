b5f9cbe76d057c03e3b8ce06aac961e5078aaabd
foxtail45 <june.27th@yahoo.com>
2023-02-17 16:04:07 +0000

    backup

---
+++ simpsonschallenge.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python3
+challenge= ["science", "turbo", ["goggles", "roof"], "fire"]
+
+a= challenge[2][1]
+b= challenge[2][0]
+c= challenge[3]
+
+print(f"The {a}! The {a}! The {a} is on {c}!")
+
+
